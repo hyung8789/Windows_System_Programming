86c6a0f27093fcd9731fda8ec9c8c3d1e873fb4e
hyung8789 <hyung8789@gmail.com>
2021-10-05 15:57:21 +0900

    no message

--- 명령 프롬프트/CmdToken.h
+++ 명령 프롬프트/CmdToken.h
@@ -3,25 +3,20 @@
 
 // CmdToken.cpp
 
-enum class CMD_TOKEN_STATE : const BOOL //명령어 토큰 상태
-{
-	//명령어 토큰 상태 관리에 대한 4비트(2^2 = 4) 필요
+//TODO : 토큰 플래그 최소화
 
-	INIT = 0, //초기 상태
-	INCOMPLETE, //불완전 상태 : 일부 할당 상태
-	COMPLETE, //완전 상태 : 전부 할당 완료 상태
-	USED //사용됨 상태 : 사용 된 토큰에 대한 상태
+enum class CMD_TOKEN_FLAG : const BOOL
+{
+	NOT_ASSIGNED = 0, //미 할당
+	ASSIGNED, //할당
+	USED //사용 됨
 };
- 
+
 struct CMD_TOKEN_FLAGS //명령어 토큰 플래그
 {
 	//최하위 비트부터 차례로 배치 (LSB -> MSB)
-
-	BOOL _cmdTokenState : 4; //명령어 토큰 상태 (MSB)
-	BOOL _isCmdTypeUsed : 1; //명령어 타입 사용 여부
-	BOOL _isCmdSubArgsUsed : 1; //명령어 하위 인자 사용 여부
-	BOOL _isCmdTypeAssigned : 1;  //명령어 타입 할당 여부
-	BOOL _isCmdSubArgsAssigned : 1; //해당 명령어에 대한 하위 인자 할당 여부 
+	CMD_TOKEN_FLAG _cmdTypeFlag : 3; //명령어 타입 플래그
+	CMD_TOKEN_FLAG _cmdSubArgsFlag : 3; //헤당 명령어에 대한 하위 인자 플래그
 };
 
 class CMD_TOKEN //명령어 토큰
