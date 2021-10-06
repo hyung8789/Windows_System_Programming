b30452b2c45bffda4d297561b583c39ba583d4b5
hyung8789 <hyung8789@gmail.com>
2021-10-06 18:58:57 +0900

    no message

--- 명령 프롬프트/CmdToken.cpp
+++ 명령 프롬프트/CmdToken.cpp
@@ -1,6 +1,5 @@
 #include "Core.h"
 
-
 CmdToken::CmdToken()
 {
 	this->ClearAll();
@@ -25,28 +24,56 @@ void CmdToken::ClearAll()
 /// 명령어 토큰 생성
 /// 명령어 파싱과정에서 하나씩 처리하므로 생성 작업 시 한 번에 하나의 작업만 수행
 /// </summary>
-/// <param name="args">명령어 토큰 생성 시 인자</param>
+/// <param name="arg">명령어 토큰 생성 시 인자</param>
 /// <returns>명령어 토큰의 현재 상태</returns>
-unsigned char CmdToken::GenerateToken(void* args)
+CMD_TOKEN_FLAGS CmdToken::GenerateToken(void* arg)
 {
-	//TODO : 명령어 토큰 생성 작업
-	//
 	/***
 		< 현재 플래그 값에 따른 토큰 생성 작업에서의 이벤트 처리 >
 
-		cmdTypeFlag |  cmdSubArgsFlag | event proc
+		CMD_TYPE_ASSIGNED || CMD_SUBARGS_ASSIGNED || current event proc
+		T					 T						clearAll (cmdType 및 cmdSubArgs 모두 EXPIRED 상태여야함), cmdTypeAssign
+		T					 F						cmdSubArgsAssign
+		F					 T						ThrowException (명령어 타입이 아니라서 하위 인자로 처리하더라도 CMD_TYPE_ASSIGNED는 T)
+		F					 F						cmdTypeAssign
 
 	***/
 
+	if (this->_cmdTokenFlags & (CMD_TYPE_ASSIGNED | CMD_SUBARGS_ASSIGNED)) //T T
+	{ 
+		if (this->_cmdTokenFlags & (CMD_TYPE_EXPIRED | CMD_SUBARGS_EXPIRED))
+		{
+			this->ClearAll(); //clearAll (EXPIRED 상태여야함)
+			//cmdTypeAssign
+		}
+		else	
+			exceptionhandler::ThrowException(exceptionhandler::EX::NOT_USED_TOKEN_EXIST);
+	}
+	else if(!this->_cmdTokenFlags & (CMD_TYPE_ASSIGNED | CMD_SUBARGS_ASSIGNED)) //F F
+	{
+		//cmdTypeAssign
+		
+	}
+	else //T F or F T
+	{
+		if(this->_cmdTokenFlags & (CMD_SUBARGS_ASSIGNED))
+			exceptionhandler::ThrowException(exceptionhandler::EX::WRONG_CMD_TYPE_FLAG);
+
+		//cmdSubArgsAssign
+	}
+
+
 CMD_TYPE_ASSIGN_PROC:
 
-	if (args == NULL)
+	if (arg == NULL)
 	{
 		//TODO : 현재 토큰 상태에 따라 예외처리 추가
 	}
 
-	this->_cmdType = CmdTypeParser::GetCmdTypeFromString((const TCHAR*)args); //명령어 타입 파싱하여 할당
-	// 명령어가 이미 할당되었지만 start start echo test와 같이 다시 명령어가 올 경우
+	this->_cmdType = CmdTypeParser::GetCmdTypeFromString((const TCHAR*)arg); //명령어 타입 파싱하여 할당
+	//토큰 생성 과정에서 명령어 타입의 토큰이 아닐 경우 하위 인자로서 처리
+
+
 
 	if (this->_cmdType == CMD_TYPE::NONE_CMD_TYPE) //하위인자로처리
 	{
@@ -56,7 +83,7 @@ CMD_TYPE_ASSIGN_PROC:
 
 
 CMD_SUB_ARGS_ASSIGN_PROC:
-	_tcscpy_s(this->_cmdSubArgs, (const TCHAR*)args); //명령어에 대한 하위 인자 복사
+	_tcscpy_s(this->_cmdSubArgs, (const TCHAR*)arg); //명령어에 대한 하위 인자 복사
 
 	return this->_cmdTokenFlags;
 }
@@ -66,10 +93,17 @@ CMD_SUB_ARGS_ASSIGN_PROC:
 /// </summary>
 enum CMD_TYPE CmdToken::GetCmdTypeOnce()
 {
-	if (this->_cmdTokenFlags & CMD_TYPE_EXPIRED) //다시 읽기 방지
-		exceptionhandler::ThrowException(exceptionhandler::EX::EXPIRED_TOKEN_ACCESS);
+	if (this->_cmdTokenFlags & CMD_TYPE_ASSIGNED)
+	{
+		if (this->_cmdTokenFlags & CMD_SUBARGS_EXPIRED) //다시 읽기 방지
+			exceptionhandler::ThrowException(exceptionhandler::EX::EXPIRED_TOKEN_ACCESS);
 
-	return this->_cmdType;
+		this->_cmdTokenFlags |= CMD_TYPE_EXPIRED;
+
+		return this->_cmdType;
+	}
+
+	exceptionhandler::ThrowException(exceptionhandler::EX::NOT_ASSIGNED_TOKEN_ACCESS);
 }
 
 /// <summary>
@@ -77,8 +111,15 @@ enum CMD_TYPE CmdToken::GetCmdTypeOnce()
 /// </summary>
 const TCHAR* CmdToken::GetCmdSubArgsOnce()
 {
-	if (this->_cmdTokenFlags & CMD_SUBARGS_EXPIRED) //다시 읽기 방지
-		exceptionhandler::ThrowException(exceptionhandler::EX::EXPIRED_TOKEN_ACCESS);
+	if (this->_cmdTokenFlags & CMD_SUBARGS_ASSIGNED)
+	{
+		if (this->_cmdTokenFlags & CMD_SUBARGS_EXPIRED) //다시 읽기 방지
+			exceptionhandler::ThrowException(exceptionhandler::EX::EXPIRED_TOKEN_ACCESS);
 
-	return this->_cmdSubArgs;
+		this->_cmdTokenFlags |= CMD_SUBARGS_EXPIRED;
+
+		return this->_cmdSubArgs;
+	}
+
+	exceptionhandler::ThrowException(exceptionhandler::EX::NOT_ASSIGNED_TOKEN_ACCESS);
 }
--- 명령 프롬프트/ExceptionHandler.h
+++ 명령 프롬프트/ExceptionHandler.h
@@ -5,8 +5,11 @@ namespace exceptionhandler
 {
 	typedef enum class EXCEPTION : const int
 	{
-		EXPIRED_TOKEN_ACCESS = -1,
-		NULL_INPUT_CMD = -2
+		NOT_USED_TOKEN_EXIST = -1,
+		WRONG_CMD_TYPE_FLAG = -2,
+		NOT_ASSIGNED_TOKEN_ACCESS = -3,
+		EXPIRED_TOKEN_ACCESS = -4,
+		NULL_INPUT_CMD = -5
 	}EX;
 
 	void ThrowException(EXCEPTION ex);
